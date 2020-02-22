d4c408791103f79774a28a35df335119c0e4a4b8
Matthew Nykamp <murnkamp@gmail.com>
2020-02-22 15:25:31 -0500

    finish worldgen

--- TileMap.gd
+++ TileMap.gd
@@ -8,8 +8,8 @@ var ysize = 32
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#randomize()
-	noise.seed = 23
+	randomize()
+	noise.seed = randi()
 	noise.octaves = 3
 	noise.period =  10.0
 	
@@ -35,13 +35,13 @@ func _ready():
 			var inputVal = 9
 			
 			if (grid[n][m] != -1):
-				if (n==0 or grid[n-1][m]==-1):
+				if (n==0 or grid[n-1][m]==-1 or grid[n-1][m]==8):
 					emptyL =  true
-				if (n==xsize-1 or grid[n+1][m]==-1):
+				if (n==xsize-1 or grid[n+1][m]==-1 or grid[n+1][m]==8):
 					emptyR = true
-				if (m==0 or grid[n][m-1]==-1):
+				if (m==0 or grid[n][m-1]==-1 or grid[n][m-1]==8):
 					emptyU = true
-				if (m==ysize-1 or grid[n][m+1]==-1):
+				if (m==ysize-1 or grid[n][m+1]==-1 or grid[n][m+1]==8):
 					emptyD = true
 					
 				if (emptyL and emptyR and emptyU and emptyD):
@@ -76,17 +76,51 @@ func _ready():
 			else:
 				grid[n][m]=-1
 	
-	for n in randi()%3+3:
+	for n in randi()%8+3:
 		var yval = randi()%(ysize - 14) + 7
 		var xval = randi()%(xsize - 14) + 7
 		var num = randi()%3+4
 		
-		for l in range(0, num):				
-			if grid[xval+l][yval]==-1:
-				if l==0 or l==num-1:
-					grid[xval+l][yval]=0
-				else:
-					grid[xval+l][yval]=1
+		var valid = true
+		
+		for l in range(0, num):
+			if grid[xval+l][yval]!=-1:
+				valid = false
+		
+		if valid:
+			for l in range(0, num):				
+				if grid[xval+l][yval]==-1:
+					if l==0 or l==num-1:
+						grid[xval+l][yval]=0
+					else:
+						grid[xval+l][yval]=1
+						
+	var maxHeight = 8					
+	
+	for n in xsize:
+		for m in ysize:
+			if grid[n][m]==8 and m > 1:
+				var length = 0
+				for l in range(0, maxHeight):
+					if m-l < 0:
+						break
+					elif grid[n][m-l]==-1 or grid[n][m-l]==8:
+						length=length+1
+					else:
+						break
+				
+				print(length)
+				
+				if length > 2:
+					if randi()% 10 <= 1:
+						var realLength = max(3,length-randi()%6)
+						for l in range(0, realLength):
+							if l == 0:
+								grid[n][m-l]=7
+							elif l == realLength-1:
+								grid[n][m-l]=5
+							else:
+								grid[n][m-l]=6
 	
 	for n in range(0,xsize - 1):
 		for m in range(0, ysize-1):
