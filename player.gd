6515efece626a88871061a89210ae003219a5775
mcwissink <markcwissink@gmail.com>
2020-02-22 12:50:13 -0500

    fix character

--- player.gd
+++ player.gd
@@ -4,10 +4,10 @@ extends RigidBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-const MOVE_ACCEL = 1000.0
+const MOVE_ACCEL = 2000.0
 const MOVE_DEACCEL = 2000.0
 const MOVE_FALL = 2000.0
-const MOVE_MAX_VELOCITY = 3000.0
+const MOVE_MAX_VELOCITY = 500.0
 const JUMP_VELOCITY = 900.0
 var device_id = -1
 
@@ -23,9 +23,6 @@ var aim_angle
 func _ready():
 	pass # Replace with function body.
 
-func process(delta):
-	add_force(Vector2(1,0),Vector2(0,1))
-
 func _integrate_forces(state):
 	var lv = state.get_linear_velocity()
 	var step = state.get_step()
@@ -39,10 +36,10 @@ func _integrate_forces(state):
 		var ci = state.get_contact_local_normal(x)
 		if ci.dot(Vector2(0, -1)) > 0.6:
 			found_floor = true
-
+			
+	if sign(lv.x) != sign(move_x):
+		lv.x += move_x * MOVE_DEACCEL * step
 	if abs(lv.x) < MOVE_MAX_VELOCITY:
-		if sign(lv.x) != sign(move_x):
-			lv.x += move_x * MOVE_DEACCEL * step
 		lv.x += move_x * MOVE_ACCEL * step
 
 	if move_y > 0: 
